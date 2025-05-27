2022d86469fdf420be414e6bd88f22b32537ae00
Thiago Monteiro <thiagomonteiro.developer@gmail.com>
2025-05-26 22:41:21 -0300

    [ ♻️ refacgtory ]: Removing unnecessary validation inside `MainViewControllerSpy`

--- CleanArchitectureTests/Domain/Scenes/Main/Helpers/Spy/MainViewControllerSpy.swift
+++ CleanArchitectureTests/Domain/Scenes/Main/Helpers/Spy/MainViewControllerSpy.swift
@@ -12,19 +12,11 @@ final class MainViewControllerSpy: MainViewControllerDisplayableLogic {
     private(set) var displayCepDataCalled: Bool = false
     private(set) var displayCepDataCount: Int = 0
     
-    private(set) var requestCepDataCalled: Bool = false
-    private(set) var requestCepDataCount: Int = 0
-    private(set) var expectedCep: String?
-    
     func displayCepData(_ viewModel: CepRequestModel.ViewModel) async throws {
         displayCepDataCalled = true
         displayCepDataCount += 1
         expectedDisplayCepData = viewModel
     }
     
-    func requestCepData(_ cep: String) async throws {
-        requestCepDataCalled = true
-        requestCepDataCount += 1
-        expectedCep = cep
-    }
+    func requestCepData(_ cep: String) async throws {}
 }
